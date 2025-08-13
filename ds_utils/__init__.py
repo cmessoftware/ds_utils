c9073f93770953fac95e1081ce4e56761679c641
Sergio Salanitri <cmessoftware@gmail.com>
2025-08-13 17:24:37 -0300

    Configuraación para se isntaldo desde un notebbok

--- ds_utils/__init__.py
+++ ds_utils/__init__.py
@@ -10,10 +10,6 @@ from .viz import Viz
 from .mlflow_utils import MLflowUtils
 #Note: Modify analyze_target_patterns to get current target_col features.
 
-#TODO: Futuros módulos
-# from .viz import plot_matriz_confusion
-# from .mlflow_utils import iniciar_mlflow, log_metricas_basicas
-
 __all__ = [
     "DataLoader",
     "MetricsCalculator",
